8ff89278faa1fedacc0f2f4422d2f0236dd64439
Tristan Perera <tristan.e.perera@gmail.com>
2018-08-26 13:26:04 -0700

    minor changes

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-var arr = ["quote1", "quote two", "third quote", "a 4th quote", "number 5", "sixth", "lastly number 7 quote"];
-var nameArr = ["testone"];
-
-// var rand300 =  [Math.floor((Math.random() * 300) + 1)]
-// var rand650 =  [Math.floor((Math.random() * 650) + 1)]
-
-$("#demo").click(function () {
-  // alert("jquery is working");
-  $("#quote").html("<p>" + arr[Math.floor((Math.random() * 7) + 0)] + "</p>");
-
-});
-
-
+//CREATING THE URL
 var api = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=";
 var stock = "input";
-var apiKey = "&apikey=N06BDYLYWYZK0MA6";
-
-
+var apiKey = "&apikey=N06BDYLYWYZK0MA6"; //PRIVATE KEY
 
 
 $(document).ready(function () {
@@ -29,9 +15,9 @@ $(document).ready(function () {
         type: "GET",
         url: api + stock + apiKey,
         success: function (data) {
-          let close = data["Time Series (Daily)"]["2018-08-24"]["4. close"];
-          $("#check").html(close);
-          $("#check").prepend(stock.toUpperCase() + ": $");
+          let close = data["Time Series (Daily)"]["2018-08-24"]["4. close"]; //location of closing price (need to figure out how to automatically get last price)
+          $("#check").html(close); //output to html
+          $("#check").prepend(stock.toUpperCase() + ": $"); //adds stock name
         }
       });
     }
@@ -45,5 +31,5 @@ $(document).ready(function () {
 
 
 
-// http://api.openweathermap.org/data/2.5/weather?q=whittier,ca&APPID=a87a003c40bfefe9d5fa286f9e0f5917
+// DEMO api
 // https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo
